c8806bd7a0cda17ca75d6e66442d68a4563193bf
Sachini Dissanayaka <61037110+ChathurikaDissanayaka@users.noreply.github.com>
2022-01-28 00:16:24 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,6 +2,50 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 
+// app.get("/", function(req, res) {
+//   res.send("Hello Express");
+// });
+
+absolutePath = __dirname + "/views/index.html"
+
 app.get("/", function(req, res) {
-  res.send("Hello Express");
+  res.sendFile(absolutePath);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
