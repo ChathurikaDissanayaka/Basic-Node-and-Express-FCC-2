97fd1ffe3de2ac8c4490dc9f3578338c90ed6ee7
Sachini Dissanayaka <61037110+ChathurikaDissanayaka@users.noreply.github.com>
2022-01-30 22:52:55 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -26,6 +26,13 @@ app.get("/json", function(req, res) {
   res.json({"message": "Hello json"});
 });
 
+app.get("/now", function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.send({time: req.time});
+});
+
 
 
 
